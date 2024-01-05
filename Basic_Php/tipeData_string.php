56110939968407308c9f90234c1a32c5945ee429
Royhan <royhankeren12@gmail.com>
2024-01-05 23:15:42 +0700

    string

--- Basic_Php/tipeData_string.php
+++ Basic_Php/tipeData_string.php
@@ -5,6 +5,7 @@
     // \t digunakan untuk menambahkan spasi Tab dari kata yang di berikan.
 
 
+    // single qoute
     echo 'Name : ';
     echo 'Royhan';
 
@@ -12,5 +13,33 @@
     echo "Name : ";
     echo "\nRoyhan";
 
-    echo "\nNama\t Saya\t Royhan";
+    // double qoute
+    echo "\nNama\t Saya\t Royhan\n";
+
+    
+    /**
+     * HEREDOC.
+     * Heredoc adalah fitur untuk membuat string lebih panjang tanpa menggunakan \n,\t,"",dan '' .
+     * jadi kita tinggal menuliskan text nya seperti biasa saja menggunakan spasi ataupun enter.
+     * kita tinggal membuat fiturnya menggunakan <<< nama_variabel(data yang di isi)nama_variabel;
+     * contohnya seperti berikut.
+     */
+
+    echo <<<ROYHAN
+    perkenalkan nama saya royhan dan saya kuliah di universitas budi darma medan,saat ini saya 
+    mempelajari basic basic dan fundamental dari bahasa pemograman php.
+    ROYHAN;
+
+    /**
+     * NOWDOC.
+     * NOWDOC sama seperti HEREDOC yang membedakan hanya lah pada penamaan variabel.
+     * di NOWDOC harus di kasih petik '',sedangkan di HEREDOC tidak perlu dikasih ''.
+     * hasil yang di munculkan juga tetap sama.
+     */
+
+     echo <<<'ROYHAN'
+     perkenalkan nama saya royhan dan saya kuliah di universitas budi darma medan,saat ini saya 
+     mempelajari basic basic dan fundamental dari bahasa pemograman php.
+     ROYHAN;
+
 ?>
