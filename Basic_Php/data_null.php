a1e7a6575e4267967b1ceb457ed7a41e91232406
Royhan <royhankeren12@gmail.com>
2024-01-20 10:05:05 +0700

    data_null

---
+++ Basic_Php/data_null.php
@@ -0,0 +1,7 @@
+<?php
+$name = "Eko";
+$name = null;
+echo "Is name Null? : ";
+ var_dump(is_null($name)); // <- pasti hasilnya akan true karena variabel dari $name ialah null
+// ketika kita mengisi datanya maka hasilnya akan false,dikarenakan tidak null
+echo "\n"; //<- untuk membuat enter ke bawah
