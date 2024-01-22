676b176ed44d54d05cbb8a7c3bc27b5de33843ac
Royhan <royhankeren12@gmail.com>
2024-01-22 22:03:58 +0700

    increment_decrement

---
+++ Basic_Php/increment_decrement.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Increment Dan Decrement.
+ * sama seperti penggunaan bahasa pemograman lain
+ * contoh nya seperti berikut :
+ */
+
+// salah satu contoh penggunaan increment;
+$a = 0;
+var_dump(++$a); // <- otomatis akan mengembalikan nilai $a yaitu 0,dan menaikkan 1 angka;
+// atau
+
+var_dump($a++); // <- sedangkan ini ialah post increment
+
+$b = 1;
+// salah satu contoh penggunaan decrement;
+var_dump(--$b); // <- otomatis akan mengembalikan nilai $b yaitu 1,dan menurunkan 1 angka;
+// atau
+var_dump($b--);
+
+
