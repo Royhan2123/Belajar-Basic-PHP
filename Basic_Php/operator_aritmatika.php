0fbfbaf966fc204f0c148a8e93699e582f5ac67b
Royhan <royhankeren12@gmail.com>
2024-01-22 20:22:45 +0700

    Operator Aritmatika

---
+++ Basic_Php/operator_aritmatika.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ *  Disini kita akan membuat sebuah operasi aritmatika
+ *  sama seperti bahasa pemgoraman lainnya,yang membedekan ialah
+ *  dalam pemograman php ada pangkat (**)
+ */
+
+$pertambahan = 10 + 10;
+echo $pertambahan;
+var_dump($pertambahan);
+echo "\n";
+
+//Perkurangan
+$perkurangan = 10 - 10;
+echo $perkurangan;
+var_dump($perkurangan);
+echo "\n";
+
+// Perkalian
+$perkalian = 10 * 10;
+echo $perkalian;
+var_dump($perkalian);
+echo "\n";
+
+// Pembagian
+$pembagian = 10 / 10;
+echo $pembagian;
+var_dump($pembagian);
+echo "\n";
+
+// Sisa Bagi
+$sisaBagi = 10 % 10;
+echo $sisaBagi;
+var_dump($sisaBagi);
+echo "\n";
+
+// Pangkat
+$pangkat = 10 ** 2;
+echo $pangkat;
+var_dump($pangkat);
+echo "\n";
+
+// Minus
+$minus = -10;
+echo $minus;
+var_dump($minus);
+echo "\n";
