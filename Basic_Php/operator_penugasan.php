05f07b726c2fc7a95552ac6f601edde902dcb4fb
Royhan <royhankeren12@gmail.com>
2024-01-22 20:34:18 +0700

    Operator Penugasan

---
+++ Basic_Php/operator_penugasan.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Operator Penugasan ialah sebuah operator yang menggunakan += langsung.
+ * contohnya seperti.
+ */
+
+// Pertambahan
+$total = 0;
+
+$tambah = 10;
+$kurang = 10;
+$total+=$tambah; // inputnya adalah total = 0 + dengan tambah yaitu = 10 yang hasilnya 0;
+echo "$total\n"; //  dan output nya adalah 10;
+var_dump($tambah); // yang berbentuk int = 10;
