71adef2d822dce304fa2b7d7ba4c744e531ac958
Royhan <royhankeren12@gmail.com>
2024-01-23 15:44:26 +0700

    null coelising operator

---
+++ Basic_Php/null_coelising_operator.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * DISINI KITA AKAN MENGECEK DATA YANG NULL,TETAPI KITA BUAT TIPE DATANYA SENDIRI
+ * WALAUPUN TIDAK ADA.
+ * SEPERTI CONTOH KODE BERIKUT :
+ */
+
+
+$data = [];
+$action = $data["Action"] ?? "noting"; // <- ?? digunakan untuk mendapatkan nilai default
+// jika nilai yang didapatkan null.
+// jadi otomatis hasil di atas ialah noting,karena array action di $data tidak ada.
+
+echo $action;
+
+
